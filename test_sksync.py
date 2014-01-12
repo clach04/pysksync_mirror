d6ea457d0701c17a5400bbc034ed891a07916fc4
clach04 <424032+clach04@users.noreply.github.com>
2014-01-11 16:39:20 -0800

    Testing passwords that do not match stored salted verification key. NOTE test currently fails due to not making a decision about what/how failures are reported. Currently the server knows there was an auth failure but simply severs the connection with the client.

--- test_sksync.py
+++ test_sksync.py
@@ -339,8 +339,6 @@ class TestSKSync(GenericSetup):
         # TODO check no other files exist in self.client_dir
 
 
-# TODO TestSKSyncWithInvalidAuth(TestSKSync):
-#        self.config['password'] = 'this is wrong'  # invalid password for below
 class TestSKSyncWithValidAuth(TestSKSync):
     """Repeat tests above but with auth."""
 
@@ -358,6 +356,34 @@ class TestSKSyncWithValidAuth(TestSKSync):
             }
         }
 
+
+class TestSKSyncWithInvalidAuth(GenericSetup):
+
+    def setUp(self):
+        GenericSetup.setUp(self)
+        self.config['require_auth'] = True
+        self.config['username'] = 'testuser'
+        self.config['password'] = 'this is wrong'  # invalid password for below
+        self.config['users'] = {
+            'testuser': {
+                'authsrp': [
+                    'cf78a7a5', 
+                    '7443843a24acb936bfb5d5e0d4184a3fd521d4edd8096cf2ac9cdc62eed1a363d9c4a1bd39cb69c8836eb6f77e757e73b77be766af8547eeab4d9b3be17e2860c81afde7d4d8b5b855635ccd22352e2538b27a30518c65e825f7bb29a7037e79aa144726af2dc24ccae76a8e7a2f97fede87aee5ecab1e1ee7e559ce85fc14767ef25314c121b9c093dcf980caab66c60ae7c426a885e04bcbd761b6289b582a6d194a145932180f9b55f58cb1d937659ded8c9eeb59490705c22263241ead65db01ac218a2b76c49947fdaf4f82c5de79c97f17da1101fc1daf14e7f49beb9b8c4496c4a585805a8b858f159ec2c8d423819f84530f496ee5303d2b2eb6a32b'
+                ]
+            }
+        }
+
+    # Cut down and modified version of TestSKSync.test_sync_from_server_with_times_to_empty_client_directory()
+    def test_sync_from_server_with_times_to_empty_client_directory(self):
+        safe_rmtree(self.client_dir)
+        safe_mkdir(self.client_dir)
+        result = os.path.isdir(self.server_dir)
+
+        # do sync
+        perform_sync(self.server_dir, self.client_dir, config=self.config)
+        # Both a server and client failure is expected
+        # However how they should be reported is not yet specified in the protocol
+        # TODO check for failure once failure reporting mechanism is specified
 
 try:
     TestSKSync.assertTrue
